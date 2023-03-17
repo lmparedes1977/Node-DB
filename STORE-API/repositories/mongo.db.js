c46d66272230770e84b0837895a7453178c7caae
Leonardo Paredes <110631380+lmparedes1977@users.noreply.github.com>
2023-03-17 14:44:51 -0300

    Update mongo.db.js

--- STORE-API/repositories/mongo.db.js
+++ STORE-API/repositories/mongo.db.js
@@ -1,16 +1,11 @@
-// import mongodb from "mongodb";
 
-// function getClient(){
-//     const uri = "mongodb+srv://lmparedes:mongo2023@cluster0.krv7pn9.mongodb.net/?retryWrites=true&w=majority";
-//     return new mongodb.MongoClient(uri);
-// }
 
 // export default { getClient };
 
 import mongoose from "mongoose";
 
 async function connect(){
-    const uri = "mongodb+srv://lmparedes:mongo2023@cluster0.krv7pn9.mongodb.net/?retryWrites=true&w=majority";
+    const uri = "string de conexão";
     return await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
